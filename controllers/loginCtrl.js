a5c50f2943c2c22c88229e8fbd9b276ecd06799d
Deepak Aklecha <54493492+deepakaklecha@users.noreply.github.com>
2023-05-17 12:10:35 +0530

    Update loginCtrl.js

--- controllers/loginCtrl.js
+++ controllers/loginCtrl.js
@@ -25,60 +25,60 @@ const login = (req, res) => {
 };
 
 // Define the callback route to handle the authorization code exchange
-// const callback = (req, res) => {
-//   const authorizationCode = req.query.code;
+const callback = (req, res) => {
+  const authorizationCode = req.query.code;
 
-//   // Request access token from LinkedIn
-//   const requestBody = querystring.stringify({
-//     grant_type: "authorization_code",
-//     code: authorizationCode,
-//     client_id: clientId,
-//     client_secret: clientSecret,
-//     redirect_uri: redirectUri,
-//   });
+  // Request access token from LinkedIn
+  const requestBody = querystring.stringify({
+    grant_type: "authorization_code",
+    code: authorizationCode,
+    client_id: clientId,
+    client_secret: clientSecret,
+    redirect_uri: redirectUri,
+  });
 
-//   const config = {
-//     headers: {
-//       "Content-Type": "application/x-www-form-urlencoded",
-//     },
-//   };
+  const config = {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  };
 
-//   axios
-//     .post("https://www.linkedin.com/oauth/v2/accessToken", requestBody, config)
-//     .then((response) => {
-//       const accessToken = response.data.access_token;
-//       // Use the access token for authenticated API requests
-//       // For example, you can make a request to the user's profile
-//       axios
-//         .get(
-//           "https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))",
-//           {
-//             headers: {
-//               Authorization: `Bearer ${accessToken}`,
-//             },
-//           }
-//         )
-//         .then((response) => {
-//           const emailData = response.data;
-//           const emailAddress = emailData.elements[0]["handle~"].emailAddress;
+  axios
+    .post("https://www.linkedin.com/oauth/v2/accessToken", requestBody, config)
+    .then((response) => {
+      const accessToken = response.data.access_token;
+      // Use the access token for authenticated API requests
+      // For example, you can make a request to the user's profile
+      axios
+        .get(
+          "https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))",
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        )
+        .then((response) => {
+          const emailData = response.data;
+          const emailAddress = emailData.elements[0]["handle~"].emailAddress;
 
-//           // Respond with the email address only
-//           res.send(emailAddress);
-//         })
-//         .catch((error) => {
-//           console.error("Error making API request:", error.response.data);
-//           res
-//             .status(500)
-//             .send("An error occurred while making the API request");
-//         });
-//     })
-//     .catch((error) => {
-//       console.error("Error getting access token:", error.response.data);
-//       res.status(500).send("An error occurred while getting the access token");
-//     });
-// };
+          // Respond with the email address only
+          res.send(emailAddress);
+        })
+        .catch((error) => {
+          console.error("Error making API request:", error.response.data);
+          res
+            .status(500)
+            .send("An error occurred while making the API request");
+        });
+    })
+    .catch((error) => {
+      console.error("Error getting access token:", error.response.data);
+      res.status(500).send("An error occurred while getting the access token");
+    });
+};
 
 module.exports = {
   login,
-//   callback,
+  callback,
 };
